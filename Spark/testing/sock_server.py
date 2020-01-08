6429ff4c190eec2f5bdce41534dc45d996a88705
GEYUNG JEN <35684497+geyungjen@users.noreply.github.com>
2020-01-08 10:59:41 -0800

    Update sock_server.py

--- Spark/testing/sock_server.py
+++ Spark/testing/sock_server.py
@@ -23,9 +23,9 @@ def get_message():
 #    value=random.randint(1,10000000)
 #    string="".join(sorted([chr(i) for i in range(ord('a'),ord('a')+26)],key=lambda x: random.randint(1,1000)))
 #    return str(datetimestamp)+","+string+","+str(value)+"\n"
-#    For testing with Spark streaming word count, use below
+#    For testing with Spark example streaming word count, use below as source of streaming
      popular_word_list=['a','about','all','also','and','as','at','be','because','but','by','can','come','could','day','do','even','find','first','for','from','get','give','go','have','he','her','here','him','his','how','I','if','in','into','it','its','just','know','like','look','make','man','many','me','more','my','new','no','not','now','of','on','one','only','or','other','our','out','people','say','see','she','so','some','take','tell','than','that','the','their','them','then','there','these','they','thing','think','this','those','time','to','two','up','use','very','want','way','we','well','what','when','which','who','will','with','would','year','you','your']
-     return ",".join(random.choices(popular_word_list,k=20))+"\n"
+     return " ".join(random.choices(popular_word_list,k=20))+"\n"
 
 
 if __name__=="__main__":
