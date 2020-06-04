925d8eaf42bb77a5dc41ab0fb733d473f66db48a
GEYUNG JEN <35684497+geyungjen@users.noreply.github.com>
2020-06-03 23:32:59 -0700

    Delete tailRecursionRLERestoreTest.scala

--- tailRecursionRLERestoreTest.scala
+++
@@ -1,6 +0,0 @@
-import com.jentekco.scala.tailRecursion
-class tailRecursionRLERestoreTest extends org.scalatest.FunSuite {
-  test("tailRecursion.rleRestore") {
-    assert(tailRecursion.rleRestore(tailRecursion.runLevelEncode("Get Uppppppppppppppppppppppppppppppppppppp!"))=="Get Uppppppppppppppppppppppppppppppppppppp!")
-  }
-}
